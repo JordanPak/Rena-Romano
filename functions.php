4042d15f474960dfec47a7d270869e30a77add41
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-23 11:50:08 -0400

    Create global scripts/styles functions; add FontAwesome

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@
 include_once( get_template_directory() . '/lib/init.php' );
 
 
+
 //-------------===--------//
 //  RENA ROMANO INCLUDES  //
 //---------------===------//
@@ -19,3 +20,26 @@ include_once( get_template_directory() . '/lib/init.php' );
 define( 'CHILD_THEME_NAME', 'RenaRomano' );
 define( 'CHILD_THEME_URL', 'http://RenaRomano.com/' );
 define( 'CHILD_THEME_VERSION', '2.0.0' );
+
+
+// ENQUEUE GLOBAL STYLES
+add_action( 'wp_enqueue_scripts', 'renaromano_global_styles' );
+function renaromano_global_styles() {
+    
+    // Google Fonts
+//    wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Droid+Serif:400,700|Roboto:400,400italic,700,700italic|Oswald', array(), CHILD_THEME_VERSION );
+    
+    // Font Awesome
+    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(), CHILD_THEME_VERSION );
+    
+} // renaromano_global_styles()
+
+
+// ENQUEUE GLOBAL SCRIPTS
+add_action( 'wp_enqueue_scripts', 'renaromano_global_scripts' );
+function renaromano_global_scripts() {
+    
+    // Pushy
+//    wp_enqueue_script( 'pushy', get_stylesheet_directory_uri() . '/inc/js/pushy.min.js', array( 'jquery' ), CHILD_THEME_VERSION );
+    
+} // renaromano_global_scripts()
