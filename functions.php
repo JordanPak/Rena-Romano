fe7cb8246995fe6aaa3c5eb5783f2e290c82317e
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-07-23 11:48:19 -0400

    Add child theme section to functions.php; set to Rena Romano

--- functions.php
+++ functions.php
@@ -4,3 +4,18 @@
 //  START GENESIS ENGINE  //
 //------------------------//
 include_once( get_template_directory() . '/lib/init.php' );
+
+
+//-------------===--------//
+//  RENA ROMANO INCLUDES  //
+//---------------===------//
+
+
+// SHORTCODES
+//require_once( CHILD_DIR . '/inc/shortcodes/file.php' );
+
+
+// CHILD THEME STUFF
+define( 'CHILD_THEME_NAME', 'RenaRomano' );
+define( 'CHILD_THEME_URL', 'http://RenaRomano.com/' );
+define( 'CHILD_THEME_VERSION', '2.0.0' );
