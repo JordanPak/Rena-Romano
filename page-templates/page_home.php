eecd4ef675a6f5a7d6f9c84ca4ecb9368e81e498
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-08-30 18:20:59 -0400

    Disable Sidebar & Loop on Home Page

--- page-templates/page_home.php
+++ page-templates/page_home.php
@@ -5,9 +5,9 @@
 
 
 // REMOVE SIDEBAR & POST CONTENT //
-//remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
 //remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
-//remove_action( 'genesis_loop', 'genesis_do_loop' );
+remove_action( 'genesis_loop', 'genesis_do_loop' );
 
 
 
