b5ea8c736c261b8e62c39c122343a7d09185ba94
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-12-16 19:02:31 -0500

    Fix Header Social Widget Area id

--- functions.php
+++ functions.php
@@ -148,7 +148,7 @@ function rena_header_contact() {
     echo '<div class="header-contact">';
 
         // Social Icons
-        genesis_widget_area( 'jimbo-social' );
+        genesis_widget_area( 'header-social' );
 
         // Phone
         echo '<a href="tel:+1-239-896-2504" class="header-contact-phone"><i class="fa fa-phone"></i>&nbsp;&nbsp;239-896-2504</a>';
@@ -162,9 +162,9 @@ function rena_header_contact() {
 
 //-- SITE-HEADER TOP WIDGET AREA //
 genesis_register_sidebar( array(
-	'id'            => 'jimbo-social',
+	'id'            => 'header-social',
 	'name'          => __( 'Site Header - Social Icons', 'renaromano' ),
-	'description'   => __( 'For Genesis Easy Social Icons', 'renaromano' ),
+	'description'   => __( 'For Simple Social Icons Widget', 'renaromano' ),
 ) );
 
 
