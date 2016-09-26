047750626aa7feead82340c2110379ad824a82d1
Jordan Pakrosnis <jpak@jordanpak.com>
2016-09-26 03:58:00 +0000

    Logo Bar Markup

--- parts/logo-bar.php
+++ parts/logo-bar.php
@@ -6,4 +6,23 @@
 
 // Get meta
 $meta = get_fields();
-// d($meta);
+
+if ( $meta['logos'] ) : ?>
+	<section class="logo-bar">
+
+		<h2>As Seen On</h2>
+
+		<section>
+			<?php foreach ( $meta['logos'] as $logo ) : if ( $logo['logo'] ) :
+
+				echo ( $logo['url'] ) ? '<a href="' . $logo['url'] . '" target="_blank">' : '<figure>'; ?>
+
+					<img src="<?php echo $logo['logo']['sizes']['medium']; ?>" alt="<?php echo ( $logo['title'] ) ? $logo['title'] : $logo['logo']['alt']; ?>" />
+
+				<?php echo ( $logo['url'] ) ? '</a>' : '</figure>';
+
+			endif; endforeach; ?>
+		</section>
+
+	</section><!-- .logo-bar -->
+<?php endif;
